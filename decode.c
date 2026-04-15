fa0ac89c4857047f0570d3e7f88659db87dbbc93
mmk9233 <marinakania4@gmail.com>
2026-04-15 19:34:41 -0400

    Completed task 3, encode.c and decode.c

--- decode.c
+++ decode.c
@@ -2,6 +2,22 @@
 //Author: Marina Kania
 ////////////////////////////////
 
+#include "cbc_lib.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+//Reads a CBC-encrypted file and writes a plaintext to stdout
+int main(int argc, char *argv[]){
+    if(argc != 2){
+        fprintf(stderr, "usage: decode from-file-name\n");
+        return EXIT_FAILURE;
+    }
 
+    if(decode(argv[1]) != 0){
+        fprintf(stderr, "FAILED\n");
+        return EXIT_FAILURE;
+    }
 
+    fprintf(stderr, "ok\n");
+    return EXIT_SUCCESS;
+}
--- encode.c
+++ encode.c
@@ -1,3 +1,22 @@
 //File: encode.c
 //Author: Marina Kania
 //////////////////////////////////
+
+#include "cbc_lib.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+//Reads from stdin and writes CBC-encrypted output to file
+int main(int argc, char *argv[]){
+    if(argc != 2){
+        fprintf(stderr, "usage: encode to-file-name\n");
+        return EXIT_FAILURE;
+    }
+    if(encode(argv[1]) != 0){
+        fprintf(stderr, "FAILED\n");
+        return EXIT_FAILURE;
+    }
+
+    fprintf(stderr, "ok\n");
+    return EXIT_SUCCESS;
+}
